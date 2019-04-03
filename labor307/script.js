2cd4c0d7e47012dd53e99ce68f948b0d71930ca1
Tkalec Nino <nino.tkalec@blnet.lokal>
2019-04-03 16:07:28 +0200

    animation

--- labor307/script.js
+++ labor307/script.js
@@ -1,25 +1,27 @@
 $("#klein img[alt=ac").css("opacity", "0.4");
-$("#klein img[alt=ac]").on("click", function() {
-    $("#klein img").css("opacity", "1");
-    $(this).css("opacity", "0.4");
-    $("#gross img").removeAttr("src");
-    $("#gross img").removeAttr("alt");
-    $("#gross img").attr("src", "img/assassin's_creed.jpg");
-    $("#gross img").attr("alt", "ac");
-});
-$("#klein img[alt=avatar]").on("click", function() {
-    $("#klein img").css("opacity", "1");
-    $(this).css("opacity", "0.4");
-    $("#gross img").removeAttr("src");
-    $("#gross img").removeAttr("alt");
-    $("#gross img").attr("src", "img/avatar.jpg");
-    $("#gross img").attr("alt", "avatar");
-});
-$("#klein img[alt=got]").on("click", function() {
-    $("#klein img").css("opacity", "1");
-    $(this).css("opacity", "0.4");
-    $("#gross img").removeAttr("src");
-    $("#gross img").removeAttr("alt");
-    $("#gross img").attr("src", "img/game_of_thrones.jpg");
-    $("#gross img").attr("alt", "got");
+$(document).ready(function() {
+    $("#klein img[alt=ac]").on("click", function() {
+        $("#klein img").css("opacity", "1");
+        $(this).css("opacity", "0.4");
+        $("#gross img").removeAttr("src");
+        $("#gross img").removeAttr("alt");
+        $("#gross img").attr("src", "img/assassin's_creed.jpg");
+        $("#gross img").attr("alt", "ac");
+    });
+    $("#klein img[alt=avatar]").on("click", function() {
+        $("#klein img").css("opacity", "1");
+        $(this).css("opacity", "0.4");
+        $("#gross img").removeAttr("src");
+        $("#gross img").removeAttr("alt");
+        $("#gross img").attr("src", "img/avatar.jpg");
+        $("#gross img").attr("alt", "avatar");
+    });
+    $("#klein img[alt=got]").on("click", function() {
+        $("#klein img").css("opacity", "1");
+        $(this).css("opacity", "0.4");
+        $("#gross img").removeAttr("src");
+        $("#gross img").removeAttr("alt");
+        $("#gross img").attr("src", "img/game_of_thrones.jpg");
+        $("#gross img").attr("alt", "got");
+    });
 });

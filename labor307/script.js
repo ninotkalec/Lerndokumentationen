7d565c2ef316db3c367ba0448004644897fb2204
Nino Tkalec <nino.tkalec@bbz-sh.ch>
2019-04-05 22:50:31 +0200

    übergang

--- labor307/script.js
+++ labor307/script.js
@@ -1,27 +1,39 @@
-$("#klein img[alt=ac").css("opacity", "0.4");
+$("#klein img:first-child").css("opacity", "0.4");
 $(document).ready(function() {
     $("#klein img[alt=ac]").on("click", function() {
         $("#klein img").css("opacity", "1");
         $(this).css("opacity", "0.4");
-        $("#gross img").removeAttr("src");
-        $("#gross img").removeAttr("alt");
-        $("#gross img").attr("src", "img/assassin's_creed.jpg");
-        $("#gross img").attr("alt", "ac");
+        $("#gross img").fadeOut(700);
+        setTimeout(function(){
+        $("#gross img").removeAttr("src")
+        .removeAttr("alt")
+        .attr("src", "img/assassin's_creed.jpg")
+        .attr("alt", "ac")
+        .fadeIn(700);
+}, 700);
     });
     $("#klein img[alt=avatar]").on("click", function() {
         $("#klein img").css("opacity", "1");
         $(this).css("opacity", "0.4");
-        $("#gross img").removeAttr("src");
-        $("#gross img").removeAttr("alt");
-        $("#gross img").attr("src", "img/avatar.jpg");
-        $("#gross img").attr("alt", "avatar");
+        $("#gross img").fadeOut(700);
+        setTimeout(function(){
+        $("#gross img").removeAttr("src")
+        .removeAttr("alt")
+        .attr("src", "img/avatar.jpg")
+        .attr("alt", "avatar")
+        .fadeIn(700);
+}, 700);
     });
     $("#klein img[alt=got]").on("click", function() {
         $("#klein img").css("opacity", "1");
         $(this).css("opacity", "0.4");
-        $("#gross img").removeAttr("src");
-        $("#gross img").removeAttr("alt");
-        $("#gross img").attr("src", "img/game_of_thrones.jpg");
-        $("#gross img").attr("alt", "got");
+        $("#gross img").fadeOut(700);
+        setTimeout(function(){
+        $("#gross img").removeAttr("src")
+        .removeAttr("alt")
+        .attr("src", "img/game_of_thrones.jpg")
+        .attr("alt", "got")
+        .fadeIn(700);
+}, 700);
     });
 });

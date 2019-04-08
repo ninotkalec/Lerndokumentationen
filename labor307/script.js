833323d8e5a43b1017c27227543aae1a7b27a2b0
Tkalec Nino <nino.tkalec@blnet.lokal>
2019-04-08 15:28:23 +0200

    fertig

--- labor307/script.js
+++ labor307/script.js
@@ -1,39 +1,48 @@
-$("#klein img:first-child").css("opacity", "0.4");
 $(document).ready(function() {
-    $("#klein img[alt=ac]").on("click", function() {
-        $("#klein img").css("opacity", "1");
-        $(this).css("opacity", "0.4");
-        $("#gross img").fadeOut(700);
+    $("#unten img[alt=ac]").on("click", function() {
+  // Wenn man im unteren Div auf das Bild mit dem Wert des Alt- Attributs ac klickt, wird die folgende Funktion ausgeführt:
+        $("#unten img").css("opacity", "1");
+  //Zuerst werden allen Bildern im unteren Div die Opacity normalgesetzt 
+        $(this).css("opacity", "0.2");
+  //Das Bild, das angeklickt wurde, wird eine Opacity hinzugefügt
+        $("#oben img").fadeOut(250);
+  //Das Bild im oberen Div hat ein FadeOut
         setTimeout(function(){
-        $("#gross img").removeAttr("src")
+  //Damit Das Bild nicht geändert wird, sobald man auf ein Bild klickt, sondern erst wenn das erste Bild "weg-gefadet" ist, benutzt man eine setTimeout-Funktion
+        $("#oben img").removeAttr("src")
         .removeAttr("alt")
-        .attr("src", "img/assassin's_creed.jpg")
+  //Sobald das Bild weg ist, werden das src- und alt-Attribut des Bildes im oberen Div entfernt
+        .attr("src", "https://www.quneplay.de/wp-content/uploads/2015/04/540654.jpg")
         .attr("alt", "ac")
-        .fadeIn(700);
-}, 700);
+  //Im gleichen Moment werden dem Bild im oberen Div das src- und alt-Attribut des angeklickten Bild hinzugefügt
+        .fadeIn(250);
+  //Danach "fadet" das Bild ins obere Div hinein
+}, 250);
+  //Das "300" gehört noch zur setTimeout-Funktion. Es besagt, wie lange es warten soll, bevor es die Funktionen ausführt, also genau so lang, wie der fadeOut des oberen Bildes.
     });
-    $("#klein img[alt=avatar]").on("click", function() {
-        $("#klein img").css("opacity", "1");
-        $(this).css("opacity", "0.4");
-        $("#gross img").fadeOut(700);
+  //Das wiederholt man für alle anderen Bilder des unteren Divs:
+    $("#unten img[alt=avatar]").on("click", function() {
+        $("#unten img").css("opacity", "1");
+        $(this).css("opacity", "0.2");
+        $("#oben img").fadeOut(250);
         setTimeout(function(){
-        $("#gross img").removeAttr("src")
+        $("#oben img").removeAttr("src")
         .removeAttr("alt")
-        .attr("src", "img/avatar.jpg")
+        .attr("src", "https://resi.ze-robot.com/dl/li/live-action-legend-of-aang-concept-art-by-john-staub-3840%C3%972160.jpg")
         .attr("alt", "avatar")
-        .fadeIn(700);
-}, 700);
+        .fadeIn(250);
+}, 250);
     });
-    $("#klein img[alt=got]").on("click", function() {
-        $("#klein img").css("opacity", "1");
-        $(this).css("opacity", "0.4");
-        $("#gross img").fadeOut(700);
+    $("#unten img[alt=got]").on("click", function() {
+        $("#unten img").css("opacity", "1");
+        $(this).css("opacity", "0.2");
+        $("#oben img").fadeOut(250);
         setTimeout(function(){
-        $("#gross img").removeAttr("src")
+        $("#oben img").removeAttr("src")
         .removeAttr("alt")
-        .attr("src", "img/game_of_thrones.jpg")
+        .attr("src", "https://images8.alphacoders.com/995/thumb-1920-995241.png")
         .attr("alt", "got")
-        .fadeIn(700);
-}, 700);
+        .fadeIn(250);
+}, 250);
     });
 });
